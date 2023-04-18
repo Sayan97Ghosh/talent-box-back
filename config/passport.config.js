999f3cac7bb2e1009d9bd5445788e0c61d6800c3
Sayan97Ghosh <sayanghosh97x@gmail.com>
2023-04-18 22:24:35 +0530

    new_change

--- config/passport.config.js
+++ config/passport.config.js
@@ -8,7 +8,7 @@ module.exports = (passport) => {
     passport.use(new GoogleStrategy({
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET_KEY,
-        callbackURL: 'http://localhost:1234/google/callback',
+        callbackURL: 'https://curious-mite-outerwear.cyclic.app/google/callback/',
         prompt: 'consent',
         scope: ['profile', 'email'],
         state: true
